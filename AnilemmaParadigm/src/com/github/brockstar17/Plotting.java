a47a8780876534893316e5e52ca7c1ae86846f0b
Owen McClure <omcclu@gmail.com>
2017-04-15 13:23:08 -0400

    formatting

--- AnilemmaParadigm/src/com/github/brockstar17/Plotting.java
+++ AnilemmaParadigm/src/com/github/brockstar17/Plotting.java
@@ -9,18 +9,18 @@ public class Plotting {
 	static List<Integer> line2 = new ArrayList<Integer>();
 	static int xs, ys;
 
-	public static void dataProcess(int[] points){
-		//0-65535		
-		//683x683
-		
-		for(int i = 0; i <= points.length; i++){
-			
-			line.add(683*(points[i]/65535));
-			
-			if(i % 4 == 0 && i != 0){				
-				xs = (line.get(line.size()-1)-1) - (line.get(line.size()-1)-3);
-				ys = (line.get(line.size()-1)) - (line.get(line.size()-1)-2);
-				line.add((int) Math.sqrt((xs*xs) + (ys*ys)));
+	public static void dataProcess(int[] points) {
+		// 0-65535
+		// 683x683
+
+		for (int i = 0; i <= points.length; i++) {
+
+			line.add(683 * (points[i] / 65535));
+
+			if (i % 4 == 0 && i != 0) {
+				xs = (line.get(line.size() - 1) - 1) - (line.get(line.size() - 1) - 3);
+				ys = (line.get(line.size() - 1)) - (line.get(line.size() - 1) - 2);
+				line.add((int) Math.sqrt((xs * xs) + (ys * ys)));
 			}
 		}
 	}
@@ -31,15 +31,15 @@ public class Plotting {
 		line.clear();
 		return line2;
 	}
-	
+
 	public static int[] byteProcess(byte[] b) {
-				
-		int[] array = new int[b.length/2];
-		
-		for(int i = 0; i <= b.length; i+=2){
-			array[i] = (b[i] & 0xFF) * (b[i+1] & 0xFF);
+
+		int[] array = new int[b.length / 2];
+
+		for (int i = 0; i <= b.length; i += 2) {
+			array[i] = (b[i] & 0xFF) * (b[i + 1] & 0xFF);
 		}
-		
+
 		return array;
 	}
 
