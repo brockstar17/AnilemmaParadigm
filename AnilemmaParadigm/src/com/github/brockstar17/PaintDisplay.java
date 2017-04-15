ae7fb052925eea295a065121f8332feda0535822
TSchaefer <TSchaefer@172.25.22.61>
2017-04-15 13:48:31 -0400

    updated

--- AnilemmaParadigm/src/com/github/brockstar17/PaintDisplay.java
+++ AnilemmaParadigm/src/com/github/brockstar17/PaintDisplay.java
@@ -24,9 +24,8 @@ public class PaintDisplay extends JPanel{
 		g.fillRect(0, 0, w, h);
 		//getData
 		List<Integer> l = Plotting.dataReturn();
-		//List<Line> nl = new ArrayList<Line>();
 		for(int k=0;k<l.size();k+=5){
-			Line temp = new Line(l.get(k),l.get(k+1),l.get(k+2),l.get(k+3),l.get(k+4));
+			Line temp = new Line(l.get(k)*scale,l.get(k+1)*scale,l.get(k+2)*scale,l.get(k+3)*scale,l.get(k+4));
 			temp.drawLine(g, new int[]{0,255,0});
 		}
 	}
