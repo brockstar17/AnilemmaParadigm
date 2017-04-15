7f5c261d42ddda7f0d59db46c389cc244700bead
Owen McClure <omcclu@gmail.com>
2017-04-15 11:43:22 -0400

    Merge conflict fixes

--- AnilemmaParadigm/src/com/github/brockstar17/AnilemmaParadigm.java
+++ AnilemmaParadigm/src/com/github/brockstar17/AnilemmaParadigm.java
@@ -2,17 +2,10 @@ package com.github.brockstar17;
 
 public class AnilemmaParadigm {
 
-<<<<<<< HEAD
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		new GUI();
 		new Display();
-=======
-	
-	public static void main(String[] args){
-		new GUI();
-		new Display();
-		
->>>>>>> branch 'master' of https://github.com/brockstar17/AnilemmaParadigm.git
+
 	}
-	
+
 }
--- AnilemmaParadigm/src/com/github/brockstar17/Plotting.java
+++ AnilemmaParadigm/src/com/github/brockstar17/Plotting.java
@@ -4,38 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Plotting {
-	
+
 	static List<Integer> line = new ArrayList<Integer>();
 	static int xs, ys;
-	
-	public static void dataProcess(int[] x, int[] y){
-		//0-65535		
-		//683x683
-		
-<<<<<<< HEAD
-		//sum stuff
-		return new int[]{};
-=======
-		for(int i = 0; i <= x.length; i++){
-			
-			line.add(683*(x[i]/65535));
-			line.add(683*(y[i]/65535));
-			
-			if(i % 4 == 0 && i != 0){				
-				xs = (line.get(line.size()-1)-1) - (line.get(line.size()-1)-3);
-				ys = (line.get(line.size()-1)) - (line.get(line.size()-1)-2);
-				line.add((int) Math.sqrt((xs*xs) + (ys*ys)));
+
+	public static void dataProcess(int[] x, int[] y) {
+		// 0-65535
+		// 683x683
+
+		for (int i = 0; i <= x.length; i++) {
+
+			line.add(683 * (x[i] / 65535));
+			line.add(683 * (y[i] / 65535));
+
+			if (i % 4 == 0 && i != 0) {
+				xs = (line.get(line.size() - 1) - 1) - (line.get(line.size() - 1) - 3);
+				ys = (line.get(line.size() - 1)) - (line.get(line.size() - 1) - 2);
+				line.add((int) Math.sqrt((xs * xs) + (ys * ys)));
 			}
-			
+
 		}
 	}
-	
-	public static List<Integer> dataReturn(){
-		
+
+	public static List<Integer> dataReturn() {
+
 		return line;
-		
->>>>>>> branch 'master' of https://github.com/brockstar17/AnilemmaParadigm.git
+
 	}
-	
 
 }
